1c41f6ba4dbab4c18cf689327425ee33c4279ed9
dicogaspar <wendersonhaler@hotmail.com>
2013-08-13 01:14:28 +0000

    webjs modificado

--- web.js
+++ web.js
@@ -1,1 +1,12 @@
-var msg = fs.readFileSync(index.html,utf-8)
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html','utf-8');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
